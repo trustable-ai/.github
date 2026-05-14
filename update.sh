208dfb3c277ce807b582a031070f09c8c338838c
Trustable User <noreply@example.com>
2026-05-14 17:38:32 +0100

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-IMAGE=ghcr.io/trustable-ai/trustable-app:trustable_v0.3.5_26.134.1644
+IMAGE=ghcr.io/trustable-ai/trustable-app:trustable_v0.3.5_26.134.1733
 NAMESPACE=nuvolaris
 STATEFULSET=trustable
 CONTAINER=trustable
