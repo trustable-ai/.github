5d57c487b771443021388a5441582338118f1591
Trustable User <noreply@example.com>
2026-06-22 19:28:36 +0200

    update

--- update.sh
+++ update.sh
@@ -9,13 +9,12 @@ CONTAINER=trustable
 NOTIFY="https://landing.nuvolaris.org/api/my/v1/notify?input="
 
 cd /tmp
-sudo k3s kubectl -n nuvolaris get sts/trustable -ojson >$$-1
-jq -r '.spec.template.spec.containers[].image' <$$-1 >$$-2
-RUNNING="$(awk -F: '/trustable/{print $NF}' <$$-2)"
-VERSION="$(awk -F_ '/trustable/ {print $2 }'  <$$-2)"
-TAG="$(awk -F_ '/trustable/ {print $3 }'  <$$-2)"
-rm -f "$$"-*
-echo Running: $RUNNING $VERSION $TAG
+sudo k3s kubectl -n nuvolaris get sts/trustable -ojsonpath='{range .spec.template.spec.containers[*]}{.image}{"\n"}{end}' | tee /tmp/img$$
+RUNNING="$(awk -F: '/trustable/{print $NF}' </tmp/img$$)"
+VERSION="$(awk -F_ '/trustable/ {print $2 }'  </tmp/img$$)"
+TAG="$(awk -F_ '/trustable/ {print $3 }'  </tmp/img$$)"
+rm -f /tmp/img$$
+echo Running: $RUNNING
 echo Current: $CURRENT
 
 case "$VERSION" in
