4b01cdf0f3a611a365f182092a22d8ca0e46381d
Trustable User <noreply@example.com>
2026-06-23 11:58:52 +0200

    update preview

--- update-preview.sh
+++ update-preview.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-set -euo pipefail
+sudo apt-get update && sudo apt-get -y install jq curl
 
-CURRENT=trustable_v0.3.10_26.173.1305
+TOKEN=$(curl -s "https://ghcr.io/token?scope=repository:trustable-ai/trustable-app:pull&service=ghcr.io" | jq -r '.token')
+curl -s -H "Authorization: Bearer $TOKEN" "https://ghcr.io/v2/trustable-ai/trustable-app/tags/list" |\
+jq -r '.tags[] | select(startswith("trustable_"))' | awk -F_ '{ print $3 " " $0 }' | sort -r >/tmp/releases
+CURRENT="$(head -1 _releases | awk ' {print $2}' )"
+
 IMAGE=ghcr.io/trustable-ai/trustable-app:$CURRENT
 NAMESPACE=nuvolaris
 STATEFULSET=trustable
@@ -17,25 +21,12 @@ rm -f /tmp/img$$
 echo Running: $RUNNING
 echo Current: $CURRENT
 
-case "$VERSION" in
-    (v0.3.6|v0.3.7|v0.3.8)
-        echo Version is end of life and no more updated.
-        echo Download a new installer from https://download2.trustable.it
-        curl -sL "${NOTIFY}end-of-life+$VERSION+$TAG" >/dev/null
-    ;;
-    (v0.3.9|v0.3.10)
-        if [[ "$RUNNING" == "$CURRENT" ]]
-        then echo "You are running the latest version available"
-                  curl -sL "${NOTIFY}up-to-date+$CURRENT" >/dev/null
-        else echo "Updating"
-             sudo k3s kubectl -n "$NAMESPACE" set image "statefulset/$STATEFULSET" "$CONTAINER=$IMAGE"
-             sudo k3s kubectl -n "$NAMESPACE" rollout status "statefulset/$STATEFULSET"
-             curl -sL "${NOTIFY}$RUNNING+to+$CURRENT" >/dev/null
-        fi
-    ;;
-    (*)
-        echo "Unknown version. This should not happen."
-        curl -sL "${NOTIFY}unknown+$RUNNING" >/dev/null
-    ;;
-esac
+if [[ "$RUNNING" == "$CURRENT" ]]
+then echo "You are running the latest version available"
+    curl -sL "${NOTIFY}up-to-date+$CURRENT" >/dev/null
+else echo "Updating"
+    sudo k3s kubectl -n "$NAMESPACE" set image "statefulset/$STATEFULSET" "$CONTAINER=$IMAGE"
+    sudo k3s kubectl -n "$NAMESPACE" rollout status "statefulset/$STATEFULSET"
+    curl -sL "${NOTIFY}$RUNNING+to+$CURRENT" >/dev/null
+fi
 
